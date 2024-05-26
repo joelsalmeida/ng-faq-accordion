41c816deb784e2064a8ef71af78520201ab7edb8
Joel Almeida <joel.almeida.sa@gmail.com>
2024-05-26 19:46:55 -0300

    Update app component with Faq view component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FaqViewComponent } from './faq-view/faq-view.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FaqViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
